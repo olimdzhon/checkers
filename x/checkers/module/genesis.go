5581d7506b5a31c2c4ff0a53824b6116e76530e7
Olimdzhon <write2sd@mail.ru>
2024-07-26 14:09:59 +0500

    fixed nil SystemInfo

--- x/checkers/module/genesis.go
+++ x/checkers/module/genesis.go
@@ -10,9 +10,8 @@ import (
 // InitGenesis initializes the module's state from a provided genesis state.
 func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) {
 	// Set if defined
-	if genState.SystemInfo != nil {
-		k.SetSystemInfo(ctx, *genState.SystemInfo)
-	}
+	k.SetSystemInfo(ctx, genState.SystemInfo)
+
 	// Set all the storedGame
 	for _, elem := range genState.StoredGameList {
 		k.SetStoredGame(ctx, elem)
@@ -31,7 +30,7 @@ func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
 	// Get all systemInfo
 	systemInfo, found := k.GetSystemInfo(ctx)
 	if found {
-		genesis.SystemInfo = &systemInfo
+		genesis.SystemInfo = systemInfo
 	}
 	genesis.StoredGameList = k.GetAllStoredGame(ctx)
 	// this line is used by starport scaffolding # genesis/module/export
--- x/checkers/types/genesis_test.go
+++ x/checkers/types/genesis_test.go
@@ -22,7 +22,7 @@ func TestGenesisState_Validate(t *testing.T) {
 			desc: "valid genesis state",
 			genState: &types.GenesisState{
 
-				SystemInfo: &types.SystemInfo{
+				SystemInfo: types.SystemInfo{
 					NextId: 100,
 				},
 				StoredGameList: []types.StoredGame{
